134fd953a9efb39fd262a4e46bff763022d2c4aa
nmbr73 <37710085+nmbr73@users.noreply.github.com>
2022-12-17 02:03:25 +0100

    Fix images

--- Tools/scrivconv.sh
+++ Tools/scrivconv.sh
@@ -40,23 +40,74 @@ fi
 # echo "SRCIVENER_BASE   = '$SRCIVENER_BASE'"   # 'Wiki'
 # echo "SRCIVENER_FILE   = '$SRCIVENER_FILE'"   # '_export.md'
 
+
+
+# ----------------------------------------------------------------------------
+# Write AST
+
+# pandoc --from=markdown --to=native --output="$SRCIVENER_PATH/_native.lua" $SRCIVENER_EXPORT
+
+
+
+# ----------------------------------------------------------------------------
+# Store frtont matter in environment variables
+
 pandoc --standalone --from=markdown --to plain --template=Tools/scrivconv/metadata.template.sh --output="$SRCIVENER_PATH/_frontmatter.sh" $SRCIVENER_EXPORT
 source $SRCIVENER_PATH/_frontmatter.sh
-
-
-FIRST_HEADLINE=`pandoc --lua-filter=Tools/scrivconv/first_headline.filter.lua $SRCIVENER_EXPORT`
-IMAGE_REFERENCES=`pandoc --lua-filter=Tools/scrivconv/images.filter.lua $SRCIVENER_EXPORT`
-
+rm -f $SRCIVENER_PATH/_frontmatter.sh
 
 echo "METADATA_TITLE  = '$METADATA_TITLE'"  # 'Welcome to the Kartaverse'
 echo "METADATA_AUTHOR = '$METADATA_AUTHOR'" # ''
+
+
+
+# ----------------------------------------------------------------------------
+# Get first headline from the document
+
+FIRST_HEADLINE=`pandoc --lua-filter=Tools/scrivconv/first_headline.filter.lua $SRCIVENER_EXPORT`
+
 echo "FIRST_HEADLINE  = '$FIRST_HEADLINE'"  # 'Immersive Pipeline Integration Guide'
 
+if [[ -z $FIRST_HEADLINE ]]; then
+    FIRST_HEADLINE="$METADATA_TITLE"
+fi
 
-pandoc --from=markdown --to=native --output="$SRCIVENER_PATH/_native.lua" $SRCIVENER_EXPORT
+if [[ -z $FIRST_HEADLINE ]]; then
+    FIRST_HEADLINE="Not a meaningful name at this point"
+fi
+
+
+# ----------------------------------------------------------------------------
+
+OBSIDIAN_PATH="$SRCIVENER_PATH/$FIRST_HEADLINE"
+OBSIDIAN_FILE="$FIRST_HEADLINE.md"
+OBSIDIAN_EXPORT="$OBSIDIAN_PATH/$OBSIDIAN_FILE"
+
+mkdir -p "$OBSIDIAN_PATH"
+
+IMAGE_REFERENCES=`pandoc --lua-filter=Tools/scrivconv/images.filter.lua $SRCIVENER_EXPORT`
+
+
+if [[ ! -z $IMAGE_REFERENCES ]]; then
+
+    mkdir -p "$OBSIDIAN_PATH/img"
+
+    SAVEIFS=$IFS
+    IFS=$(echo -en "\n\b")
+    for file in $IMAGE_REFERENCES
+    do
+        cp "$SRCIVENER_PATH/$file" "$OBSIDIAN_PATH/img/"
+    done
+    IFS=$SAVEIFS
+fi
+
+
+# ----------------------------------------------------------------------------
+# Run the (to be) Obsidian writer
 
 pandoc \
   --wrap=none \
   --from=markdown \
   --to=Tools/scrivconv/obsidian.writer.lua \
-  --output="$SRCIVENER_PATH/_fixed.md" $SRCIVENER_EXPORT
+  --output="$OBSIDIAN_EXPORT" $SRCIVENER_EXPORT
+
