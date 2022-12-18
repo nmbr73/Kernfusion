254dd89079090c4c279db4fd437e550f9a0e9dc2
nmbr73 <37710085+nmbr73@users.noreply.github.com>
2022-12-18 20:59:50 +0100

    Cleanup and debug mode and such That's to be able to investigate the img and index.md issues

--- Tools/scrivener_to_obsidian.sh
+++ Tools/scrivener_to_obsidian.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
 
-#SRCIVENER_EXPORT="temp/scrivener_export.md"
 DEBUG="uncommment for debug mode"
 
 SRCIVENER_EXPORT=$1
 
 if [[ -z $SRCIVENER_EXPORT ]]; then
     echo "pass path to a Scriver pandoc export as 1st arg!"
-    echo "... or just 'test'"
-    exit 10
-fi
 
-if [ $SRCIVENER_EXPORT == "test" ]; then
-
-    if [ ! -d "temp/scrivener_example.md" ]; then
-        cp -rp Data/example.export/scrivener_example.md Wiki/scrivener_example.md
+    if [ -d "Data/example.export/scrivener_example.md" ]; then
+        echo "you can for example do a "
+        echo "   cp -rp Data/example.export/scrivener_example.md temp/scrivener_example.md"
+        echo "and then call"
+        echo "   $0 temp/scrivener_example.md"
     fi
 
-    SRCIVENER_EXPORT="temp/scrivener_example.md"
-
+    exit 10
 fi
 
 
@@ -39,13 +35,16 @@ fi
 
 SRCIVENER_TMPPATH="temp"
 
-# echo "SRCIVENER_EXPORT = '$SRCIVENER_EXPORT'"  # 'temp/scrivener_export.md/scrivener_export.md'
-# echo "SRCIVENER_PATH   = '$SRCIVENER_PATH'"    # 'temp/scrivener_export.md'
-# echo "SRCIVENER_BASE   = '$SRCIVENER_BASE'"    # 'temp'
-# echo "SRCIVENER_FILE   = '$SRCIVENER_FILE'"    # 'scrivener_export.md'
-# echo "SRCIVENER_BASE   = '$SRCIVENER_TMPPATH'" # 'temp'
+if [[ ! -z $DEBUG ]]; then
+    echo "SRCIVENER_EXPORT = '$SRCIVENER_EXPORT'"  # 'temp/scrivener_export.md/scrivener_export.md'
+    echo "SRCIVENER_PATH   = '$SRCIVENER_PATH'"    # 'temp/scrivener_export.md'
+    echo "SRCIVENER_BASE   = '$SRCIVENER_BASE'"    # 'temp'
+    echo "SRCIVENER_FILE   = '$SRCIVENER_FILE'"    # 'scrivener_export.md'
+    echo "SRCIVENER_BASE   = '$SRCIVENER_TMPPATH'" # 'temp'
+    echo "SRCIVENER_TMPPATH= '$SRCIVENER_TMPPATH'" # 'temp'
+fi
 
-if [ ! -d $SRCIVENER_TMPPATH]; then
+if [ ! -d $SRCIVENER_TMPPATH ]; then
     echo "make sure to call this script from the repo base dir"
     echo "and to create a 'temp' folder in there!"
     exit 30
@@ -54,24 +53,25 @@ fi
 # ----------------------------------------------------------------------------
 # Write Original, AST, ... for debugging
 
-if [[ -z $DEBUG ]]; then
-  cp "$SRCIVENER_EXPORT" "$SRCIVENER_TMPPATH/_original.md"
-  pandoc --from=markdown --to=native --output="$SRCIVENER_TMPPATH/_native.lua" $SRCIVENER_EXPORT
+if [[ ! -z $DEBUG ]]; then
+  cp "$SRCIVENER_EXPORT" "$SRCIVENER_TMPPATH/_10_original_input.md"
+  pandoc --from=markdown --to=native --output="$SRCIVENER_TMPPATH/_20_native_AST.hs" $SRCIVENER_EXPORT
 fi
 
 # ----------------------------------------------------------------------------
 # Store front matter in environment variables
 
-pandoc --standalone --from=markdown --to plain --template=Tools/scrivener_to_obsidian/metadata.template.sh --output="$SRCIVENER_TMPPATH/_frontmatter.sh" $SRCIVENER_EXPORT
-source "$SRCIVENER_TMPPATH/_frontmatter.sh"
+pandoc --standalone --from=markdown --to plain --template=Tools/scrivener_to_obsidian/metadata.template.sh --output="$SRCIVENER_TMPPATH/_30_frontmatter.sh" $SRCIVENER_EXPORT
+source "$SRCIVENER_TMPPATH/_30_frontmatter.sh"
 
-if [[ ! -z $DEBUG ]]; then
-  rm -f "$SRCIVENER_TMPPATH/_frontmatter.sh"
+if [[ -z $DEBUG ]]; then
+  rm -f "$SRCIVENER_TMPPATH/_30_frontmatter.sh"
 fi
 
-echo "METADATA_TITLE  = '$METADATA_TITLE'"  # 'Welcome to the Kartaverse'
-echo "METADATA_AUTHOR = '$METADATA_AUTHOR'" # ''
-
+if [[ ! -z $DEBUG ]]; then
+    echo "METADATA_TITLE   = '$METADATA_TITLE'"  # 'Welcome to the Kartaverse'
+    echo "METADATA_AUTHOR  = '$METADATA_AUTHOR'" # ''
+fi
 
 
 # ----------------------------------------------------------------------------
@@ -79,7 +79,7 @@ echo "METADATA_AUTHOR = '$METADATA_AUTHOR'" # ''
 
 FIRST_HEADLINE=`pandoc --lua-filter=Tools/scrivener_to_obsidian/first_headline.filter.lua $SRCIVENER_EXPORT`
 
-echo "FIRST_HEADLINE  = '$FIRST_HEADLINE'"  # 'Immersive Pipeline Integration Guide'
+echo "FIRST_HEADLINE   = '$FIRST_HEADLINE'"  # 'Immersive Pipeline Integration Guide'
 
 if [[ -z $FIRST_HEADLINE ]]; then
     FIRST_HEADLINE="$METADATA_TITLE"
@@ -92,14 +92,23 @@ fi
 
 # ----------------------------------------------------------------------------
 
-OBSIDIAN_PATH="$SRCIVENER_PATH/_$FIRST_HEADLINE"
+OBSIDIAN_PATH="$SRCIVENER_BASE/_$FIRST_HEADLINE"
 OBSIDIAN_FILE="$FIRST_HEADLINE.md"
 OBSIDIAN_EXPORT="$OBSIDIAN_PATH/$OBSIDIAN_FILE"
+
+if [[ ! -z $DEBUG ]]; then
+    echo "OBSIDIAN_PATH    = '$OBSIDIAN_PATH'" # ''
+    echo "OBSIDIAN_FILE    = '$OBSIDIAN_FILE'" # ''
+    echo "OBSIDIAN_EXPORT  = '$OBSIDIAN_EXPORT'" # ''
+fi
 
 mkdir -p "$OBSIDIAN_PATH"
 
 IMAGE_REFERENCES=`pandoc --lua-filter=Tools/scrivener_to_obsidian/images.filter.lua $SRCIVENER_EXPORT`
 
+if [[ ! -z $DEBUG ]]; then
+    echo $IMAGE_REFERENCES > "$SRCIVENER_TMPPATH/_40_images.sh"
+fi
 
 if [[ ! -z $IMAGE_REFERENCES ]]; then
 
@@ -125,9 +134,16 @@ pandoc \
   --to=Tools/scrivener_to_obsidian/obsidian.writer.lua \
   --output="$OBSIDIAN_EXPORT" $SRCIVENER_EXPORT
 
-if [[ -z $DEBUG ]]; then
-    cp "$OBSIDIAN_EXPORT" "$SRCIVENER_TMPPATH/_pandoc.md"
+if [[ ! -z $DEBUG ]]; then
+    cp "$OBSIDIAN_EXPORT" "$SRCIVENER_TMPPATH/_50_pandoc_output.md"
 fi
 
 python Tools/scrivener_to_obsidian.py "$OBSIDIAN_EXPORT"
 
+mv "$OBSIDIAN_PATH" "Wiki/"
+
+# Now check the output in Wiki/
+# test with obsidian_to_mkdocs.sh
+# to then do a mkdocs serve to have a look
+# and finally remove the leading underscore
+# if everything is fine and should be committed
